1c6bbd4a8b5541022ef4a02cd128da75ac155239
AudriusAnusauskas <aaaudrius@gmail.com>
2023-06-30 10:53:37 +0300

    start with layout and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import Hero from "./components/Hero/Hero";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
     <>
-      <Hero />
+      <Layout />
     </>
   );
 }
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,12 @@
+import Hero from "../Hero/Hero";
+import Navbar from "../Navbar/Navbar";
+
+const Layout: React.FunctionComponent = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
+};
+export default Layout;
